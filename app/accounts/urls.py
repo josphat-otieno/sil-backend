a8cd18723c3708f43a65aa842d5c2ef2057c7479
josephat <josephatotieno92@gmail.com>
2025-09-25 13:21:51 +0300

    added endpoint to refresh JWT tokens

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import LoginView, CallbackView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns=[
     path("login/", LoginView.as_view(), name="oidc_login"),
     path("callback/", CallbackView.as_view(), name="oidc_callback"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
    
 ]
